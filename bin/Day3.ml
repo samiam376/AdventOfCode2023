103af721974ede5ccc8131533d5aafbed901d25c
samiam376 <ssheridan211@gmail.com>
2023-12-03 21:47:00 -0800

    did it
    Signed-off-by: samiam376 <ssheridan211@gmail.com>

--- bin/Day3.ml
+++ bin/Day3.ml
@@ -89,9 +89,25 @@ let capture_adj_values iter =
     adj_cords
 ;;
 
+let capture_adj_values_with_cords iter =
+  let adj_cords = get_adj_cord iter in
+  List.fold
+    ~init:[]
+    ~f:(fun acc (r, c) ->
+      let v = value_or_fail iter (r, c) in
+      (v, (r, c)) :: acc)
+    adj_cords
+;;
+
 let is_adj_to_special_char iter =
   let adj_values = capture_adj_values iter in
   List.exists adj_values ~f:(fun x -> (not (Char.is_digit x)) && not (Char.equal x '.'))
+;;
+
+let adj_asterix iter =
+  let adj_values = capture_adj_values_with_cords iter in
+  let ast = List.find adj_values ~f:(fun (x, _) -> Char.equal x '*') in
+  Option.map ast ~f:(fun (_, cords) -> cords)
 ;;
 
 let capture_number iter =
@@ -120,6 +136,46 @@ let capture_number iter =
   if is_adj then Some number, iter else None, iter
 ;;
 
+module Cord = struct
+  type t = int * int [@@deriving sexp, compare]
+end
+
+module CordMap = Map.Make (Cord)
+module CordSet = Set.Make (Cord)
+
+let capture_number2 iter cord_map =
+  let rec aux iter acc =
+    let value = get_value iter in
+    let digit =
+      match value with
+      | '0' .. '9' ->
+        let adj = adj_asterix iter in
+        Some (value, adj)
+      | _ -> None
+    in
+    let next = get_next_value iter in
+    match next, digit with
+    | None, Some d -> List.rev (d :: acc), iter
+    | Some (v, cords), Some d when Char.is_digit v ->
+      aux { iter with current_idx = cords } (d :: acc)
+    | Some (v, _), Some d when not (Char.is_digit v) -> List.rev (d :: acc), iter
+    | _ -> List.rev acc, iter
+  in
+  let captured, iter = aux iter [] in
+  let adj_asterixes = List.filter_map captured ~f:(fun (_, x) -> x) in
+  let deduped = CordSet.stable_dedup_list adj_asterixes in
+  let char_list = List.map captured ~f:(fun (x, _) -> x) in
+  let s = String.of_char_list char_list in
+  let number = Int.of_string s in
+  let map =
+    List.fold deduped ~init:cord_map ~f:(fun acc x ->
+      CordMap.update acc x ~f:(function
+        | None -> [ number ]
+        | Some l -> number :: l))
+  in
+  map, iter
+;;
+
 let traverse iter =
   let rec aux iter acc =
     let value = get_value iter in
@@ -141,171 +197,38 @@ let traverse iter =
   printf "sum: %d\n" sum
 ;;
 
-let search_left iter =
-  let rec aux iter acc =
-    if snd iter.current_idx = -1
-    then acc
-    else (
-      match get_value iter with
-      | '0' .. '9' ->
-        aux
-          { iter with current_idx = fst iter.current_idx, snd iter.current_idx - 1 }
-          (get_value iter :: acc)
-      | _ -> acc)
-  in
-  aux iter []
-;;
-
-let search_right iter =
-  let rec aux iter acc =
-    if snd iter.current_idx = snd iter.dims
-    then acc
-    else (
-      match get_value iter with
-      | '0' .. '9' ->
-        aux
-          { iter with current_idx = fst iter.current_idx, snd iter.current_idx + 1 }
-          (get_value iter :: acc)
-      | _ -> List.rev acc)
-  in
-  aux iter []
-;;
-
-let parse_number iter =
-  let current = get_value iter in
-  let left =
-    search_left { iter with current_idx = fst iter.current_idx, snd iter.current_idx - 1 }
-  in
-  let right =
-    search_right
-      { iter with current_idx = fst iter.current_idx, snd iter.current_idx + 1 }
-  in
-  let combined = left @ (current :: right) in
-  let s = String.of_char_list combined in
-  let num = Int.of_string s in
-  num
-;;
-
-let can_search_up iter = fst iter.current_idx <> 0
-let can_search_down iter = fst iter.current_idx <> fst iter.dims - 1
-let can_search_left iter = snd iter.current_idx <> 0
-let can_search_right iter = snd iter.current_idx <> snd iter.dims - 1
-
-let search_left iter =
-  if can_search_left iter
-  then (
-    let left_iter =
-      { iter with current_idx = fst iter.current_idx, snd iter.current_idx - 1 }
-    in
-    let value = get_value left_iter in
-    if Char.is_digit value then Some (parse_number left_iter) else None)
-  else None
-;;
-
-let search_right iter =
-  if can_search_right iter
-  then (
-    let right_iter =
-      { iter with current_idx = fst iter.current_idx, snd iter.current_idx + 1 }
-    in
-    let value = get_value right_iter in
-    if Char.is_digit value then Some (parse_number right_iter) else None)
-  else None
-;;
-
-let search_up iter =
-  if can_search_up iter
-  then (
-    let above_iter =
-      { iter with current_idx = fst iter.current_idx - 1, snd iter.current_idx }
-    in
-    let above_value = get_value above_iter in
-    let above_number =
-      if Char.is_digit above_value then Some (parse_number above_iter) else None
-    in
-    let numbers =
-      match above_number with
-      | Some n -> [ Some n ]
-      | None -> [ search_left above_iter; search_right above_iter ]
-    in
-    numbers)
-  else [ None ]
-;;
-
-let search_down iter =
-  if can_search_down iter
-  then (
-    let below_iter =
-      { iter with current_idx = fst iter.current_idx + 1, snd iter.current_idx }
-    in
-    let below_value = get_value below_iter in
-    let below_number =
-      if Char.is_digit below_value then Some (parse_number below_iter) else None
-    in
-    let numbers =
-      match below_number with
-      | Some n -> [ Some n ]
-      | None -> [ search_left below_iter; search_right below_iter ]
-    in
-    numbers)
-  else [ None ]
-;;
-
-(* idea find all the gears then search for numbers near by *)
-let find_adj_numbers iter =
-  let numbers =
-    search_up iter @ search_down iter @ [ search_left iter; search_right iter ]
-  in
-  let nums = List.filter_opt numbers in
-  print_endline "---";
-  printf "current idx: (%d, %d): " (fst iter.current_idx) (snd iter.current_idx);
-  List.iter nums ~f:(fun x -> printf " %d, " x);
-  print_endline "";
-  print_endline "---";
-  nums
-;;
-
-let traverse2 iter =
-  let rec aux iter acc =
+let traverse2 iter cord_map =
+  let rec aux iter cord_map =
     let value = get_value iter in
     match value with
-    | '*' ->
-      let adj_nums = find_adj_numbers iter in
-      let gear_ratio =
-        if List.length adj_nums = 2
-        then (
-          let first = List.hd_exn adj_nums in
-          let second = List.nth_exn adj_nums 1 in
-          Some (first * second))
-        else None
-      in
-      let next_cord = get_next_cord iter in
+    | '0' .. '9' ->
+      let cord_map, iter2 = capture_number2 iter cord_map in
+      let next_cord = get_next_cord iter2 in
       (match next_cord with
-       | Some c -> aux { iter with current_idx = c } (gear_ratio :: acc)
-       | None -> List.rev (gear_ratio :: acc))
+       | Some c -> aux { iter2 with current_idx = c } cord_map
+       | None -> cord_map)
     | _ ->
       (match get_next_cord iter with
-       | Some cords -> aux { iter with current_idx = cords } acc
-       | None -> List.rev acc)
+       | Some cords -> aux { iter with current_idx = cords } cord_map
+       | None -> cord_map)
   in
-  let nums = aux iter [] in
-  let nums = List.filter_opt nums in
-  let sum = List.fold ~init:0 ~f:( + ) nums in
-  printf "sum of gear ratios: %d\n" sum
+  let map = aux iter cord_map in
+  let sum =
+    CordMap.fold map ~init:0 ~f:(fun ~key:_ ~data acc ->
+      if List.length data = 2
+      then (
+        let first = List.hd_exn data in
+        let second = List.nth_exn data 1 in
+        acc + (first * second))
+      else acc)
+  in
+  printf "sum: %d\n" sum
 ;;
-
-(* let file = In_channel.read_lines "data/3a.txt" in *)
-(* let matrix = create_char_matrix file in *)
-(* (* print_char_matrix matrix; *) *)
-(* let rows = Array.length matrix in *)
-(* let cols = Array.length matrix.(0) in *)
-(* printf "rows: %d cols: %d\n" rows cols; *)
-(* let iter = { matrix; current_idx = 0, 0; dims = rows, cols } in *)
-(* traverse iter *)
 
 let file = In_channel.read_lines "data/3a.txt" in
 let matrix = create_char_matrix file in
 let rows = Array.length matrix in
 let cols = Array.length matrix.(0) in
 let iter = { matrix; current_idx = 0, 0; dims = rows, cols } in
-traverse2 iter
+let map = CordMap.empty in
+traverse2 iter map
