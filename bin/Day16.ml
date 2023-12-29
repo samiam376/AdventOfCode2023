87652c17d28e3f0e01fc0722ce3a45b6cab0274b
samiam376 <ssheridan211@gmail.com>
2023-12-28 21:13:11 -0800

    playing with imp
    Signed-off-by: samiam376 <ssheridan211@gmail.com>

--- bin/Day16.ml
+++ bin/Day16.ml
@@ -10,7 +10,7 @@ type direction =
   | Down
   | Left
   | Right
-[@@deriving sexp]
+[@@deriving sexp, equal]
 
 let inbounds (row, col) = row >= 0 && row < rows && col >= 0 && col < cols
 
@@ -29,53 +29,116 @@ let cord_key (row, col) dir =
   key
 ;;
 
+let rec beam_imp input_cords dir seen ~shadow =
+  let cords = ref input_cords in
+  try
+    while true do
+      let row = fst !cords in
+      let col = snd !cords in
+      (* printf "row: %d, col %d\n" row col; *)
+      let key = cord_key (row, col) dir in
+      let is_inbounds = inbounds (row, col) in
+      (* printf "inbounds: %b\n" is_inbounds; *)
+      let has_visited = Hashtbl.mem seen key in
+      (* printf "has visited: %b\n" has_visited; *)
+      if is_inbounds && not has_visited
+      then (
+        let next = next_cords (row, col) in
+        Hashtbl.add_exn seen ~key ~data:true;
+        shadow.(row).(col) <- '#';
+        match grid.(row).(col) with
+        | '.' -> cords := next dir
+        | '\\' ->
+          (match dir with
+           | Up -> cords := next Left
+           | Down -> cords := next Right
+           | Left -> cords := next Up
+           | Right -> cords := next Down)
+        | '/' ->
+          (match dir with
+           | Up -> cords := next Right
+           | Down -> cords := next Left
+           | Left -> cords := next Down
+           | Right -> cords := next Up)
+        | '|' ->
+          (match dir with
+           | Up | Down -> cords := next dir
+           | Left | Right ->
+             printf "splitting | \n";
+             let up = next Up in
+             beam_imp up Up seen ~shadow;
+             let down = next Down in
+             beam_imp down Down seen ~shadow)
+        | '-' ->
+          (match dir with
+           | Left | Right -> cords := next dir
+           | Up | Down ->
+             printf "splitting -\n";
+             let left = next Left in
+             beam_imp left Left seen ~shadow;
+             let right = next Right in
+             beam_imp right Right seen ~shadow)
+        | _ -> failwith "unexpected char")
+      else raise Exit
+    done
+  with
+  | Exit -> printf "exiting\n"
+;;
+
 let rec beam (row, col) dir seen ~shadow =
   let key = cord_key (row, col) dir in
-  if inbounds (row, col) && not (Set.mem seen key)
+  if inbounds (row, col) && not (Hashtbl.mem seen key)
   then (
+    printf "at row: %d, col: %d\n" row col;
     let value = grid.(row).(col) in
     let next = next_cords (row, col) in
-    let updated = Set.add seen key in
+    Hashtbl.add_exn seen ~key ~data:true;
     shadow.(row).(col) <- '#';
     match value with
-    | '.' -> beam ~shadow (next dir) dir updated
+    | '.' -> beam ~shadow (next dir) dir seen
     | '|' ->
       (match dir with
-       | Up | Down -> beam ~shadow (next dir) dir updated
+       | Up | Down -> beam ~shadow (next dir) dir seen
        | Left | Right ->
-         beam ~shadow (next Up) Up updated;
-         beam ~shadow (next Down) Down updated)
+         beam ~shadow (next Up) Up seen;
+         beam ~shadow (next Down) Down seen)
     | '-' ->
       (match dir with
-       | Left | Right -> beam ~shadow (next dir) dir updated
+       | Left | Right -> beam ~shadow (next dir) dir seen
        | Up | Down ->
-         beam ~shadow (next Right) Right updated;
-         beam ~shadow (next Left) Left updated)
+         beam ~shadow (next Right) Right seen;
+         beam ~shadow (next Left) Left seen)
     | '\\' ->
       (match dir with
-       | Up -> beam ~shadow (next Left) Left updated
-       | Down -> beam ~shadow (next Right) Right updated
-       | Left -> beam ~shadow (next Up) Up updated
-       | Right -> beam ~shadow (next Down) Down updated)
+       | Up -> beam ~shadow (next Left) Left seen
+       | Down -> beam ~shadow (next Right) Right seen
+       | Left -> beam ~shadow (next Up) Up seen
+       | Right -> beam ~shadow (next Down) Down seen)
     | '/' ->
       (match dir with
-       | Up -> beam ~shadow (next Right) Right updated
-       | Down -> beam ~shadow (next Left) Left updated
-       | Left -> beam ~shadow (next Down) Down updated
-       | Right -> beam ~shadow (next Up) Up updated)
+       | Up -> beam ~shadow (next Right) Right seen
+       | Down -> beam ~shadow (next Left) Left seen
+       | Left -> beam ~shadow (next Down) Down seen
+       | Right -> beam ~shadow (next Up) Up seen)
     | c -> failwith ("unexpected char: " ^ String.of_char c))
   else ()
 ;;
 
-let shadow_grid = Array.make_matrix ~dimx:rows ~dimy:cols '.';;
+let shadow_grid = Array.make_matrix ~dimx:rows ~dimy:cols '.'
+let table = Hashtbl.create (module String);;
 
-beam (0, 0) Right String.Set.empty ~shadow:shadow_grid
+beam (0, 0) Right table ~shadow:shadow_grid;;
+
+(* beam_imp (0, 0) Right table ~shadow:shadow_grid;; *)
+printf "done\n"
 
 let print_grid arr =
   Array.iter arr ~f:(fun row ->
     Array.iter row ~f:(fun col -> printf "%c" col);
     printf "\n")
 ;;
+
+print_grid shadow_grid
 
 let sum =
   shadow_grid
